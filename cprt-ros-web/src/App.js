b2f0adfa904ee3c5e7e0b20c8f31ffdd6f5da2c6
Mark Mckessock <mark.mckessock@gmail.com>
2021-10-14 17:10:57 -0400

    Add StreamViewer component and connection dialogue

--- cprt-ros-web/src/App.js
+++ cprt-ros-web/src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import ROSLIB from 'roslib';
-import Test from './components/Test';
+import StreamViewer from './components/StreamViewer';
 import './App.css';
+import { TextField, Button, Grid, Paper, Box }  from '@mui/material';
 
 class App extends React.Component {
   state = {
     connected: false,
     error: undefined,
     url: "ws://192.168.2.2:9090",
+    address: "localhost",
   }
 
   ros = null;
+
+  handleChange = event => this.setState({
+    address: event.target.value
+  })
+  
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.address){
+      this.setState({connected: true})
+    }
+  }
 
   handleConnect = () => {
     console.log("Attemping Connection");
@@ -49,16 +62,35 @@ class App extends React.Component {
   }
 
   render(){
-    if (this.state.connected) {
+    if (!this.state.connected) {
       return (
-        <Test ros={this.ros} topic="/chatter" type="std_msgs/String"/>
+        <Grid container justifyContent="center" alignItems="center" style={{ minHeight: '100vh' }}>
+          <Grid item lg={4} md={6} sm={8}>
+            <Paper>
+              <Box p={1}>
+                <form onSubmit={this.handleSubmit}>
+                <Grid container direction="row" spacing={2} justifyContent="left" alignItems="center">
+                  <Grid item xs={9}>
+                    <TextField required fullWidth id="address" onChange={this.handleChange} value={this.state.address} label="Rover IP"/>
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Button fullWidth type="submit" variant="contained">Connect</Button>
+                  </Grid>
+                  </Grid>
+                </form>
+              </Box>
+            </Paper>
+          </Grid>
+        </Grid>
+        
       );
     }
     else
       return (
-        <button onClick={this.handleConnect} value="Connect">
-          Connect
-        </button>
+        <div>
+          <StreamViewer host={this.state.address} port={8081} topic="/camera/image_raw" quality={80}/>
+          <StreamViewer host={this.state.address} port={8081} topic="/zed2/depth/depth_registered" quality={80}/>
+        </div>
       )
   }
 }
---
+++ cprt-ros-web/src/components/StreamViewer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import MJPEGCANVAS from 'mjpegcanvas';
+import uniqueId from 'lodash/uniqueId';
+
+class StreamViewer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.id = uniqueId("mjpeg-");
+  }
+
+  componentDidMount() {
+    var viewer = new MJPEGCANVAS.Viewer({
+      divID : this.id,
+      host : this.props.host,
+      port: this.props.port,
+      width: 320,
+      height: 240,
+      // height : this.refs.child.parentNode.clientHeight,
+      // width : this.refs.child.parentNode.clientWidth,
+      topic : this.props.topic,
+      interval : 30,
+      quality: this.props.quality
+    })
+  }
+  
+  render(){
+      return <div id={this.id} ref="child"/>
+  }
+}
+
+export default StreamViewer;
