71b5aaa6e8bcb8a03dfa6b705ca3461d0240a879
joshbob999 <joshbob38@gmail.com>
2021-10-21 11:02:45 -0400

    change pages when connected

--- cprt-ros-web/src/App.js
+++ cprt-ros-web/src/App.js
@@ -3,6 +3,7 @@ import ROSLIB from 'roslib';
 import StreamViewer from './components/StreamViewer';
 import './App.css';
 import { TextField, Button, Grid, Paper, Box }  from '@mui/material';
+import { Switch, BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 class App extends React.Component {
   state = {
@@ -62,36 +63,46 @@ class App extends React.Component {
   }
 
   render(){
-    if (!this.state.connected) {
+    // if (!this.state.connected) {
       return (
-        <Grid container justifyContent="center" alignItems="center" style={{ minHeight: '100vh' }}>
-          <Grid item lg={4} md={6} sm={8}>
-            <Paper>
-              <Box p={1}>
-                <form onSubmit={this.handleSubmit}>
-                <Grid container direction="row" spacing={2} justifyContent="left" alignItems="center">
-                  <Grid item xs={9}>
-                    <TextField required fullWidth id="address" onChange={this.handleChange} value={this.state.address} label="Rover IP"/>
-                  </Grid>
-                  <Grid item xs={3}>
-                    <Button fullWidth type="submit" variant="contained">Connect</Button>
-                  </Grid>
-                  </Grid>
-                </form>
-              </Box>
-            </Paper>
+        <Router>
+          <Switch>
+            <Route path="/ROS">
+            <div>
+              <StreamViewer host={this.state.address} port={8081} topic="/camera/image_raw" quality={80}/>
+              <StreamViewer host={this.state.address} port={8081} topic="/zed2/depth/depth_registered" quality={80}/>
+            </div>
+            </Route>
+          </Switch>
+
+          <Grid container justifyContent="center" alignItems="center" style={{ minHeight: '100vh' }}>
+            <Grid item lg={4} md={6} sm={8}>
+              <Paper>
+                <Box p={1}>
+                  <form onSubmit={this.handleSubmit}>
+                  <Grid container direction="row" spacing={2} justifyContent="left" alignItems="center">
+                    <Grid item xs={9}>
+                      <TextField required fullWidth id="address" onChange={this.handleChange} value={this.state.address} label="Rover IP"/>
+                    </Grid>
+                    <Grid item xs={3}>
+                      <Button fullWidth type="submit" variant="contained" href="ROS">Connect</Button>
+                    </Grid>
+                    </Grid>
+                  </form>
+                </Box>
+              </Paper>
+            </Grid>
           </Grid>
-        </Grid>
-        
+        </Router>
       );
-    }
-    else
-      return (
-        <div>
-          <StreamViewer host={this.state.address} port={8081} topic="/camera/image_raw" quality={80}/>
-          <StreamViewer host={this.state.address} port={8081} topic="/zed2/depth/depth_registered" quality={80}/>
-        </div>
-      )
+    // }
+    // else
+    //   return ( // Shape this to make it look nicer. But also change the button to call this instead of it just changing to this
+    //     <div>
+    //       <StreamViewer host={this.state.address} port={8081} topic="/camera/image_raw" quality={80}/>
+    //       <StreamViewer host={this.state.address} port={8081} topic="/zed2/depth/depth_registered" quality={80}/>
+    //     </div>
+    //   )
   }
 }
 
